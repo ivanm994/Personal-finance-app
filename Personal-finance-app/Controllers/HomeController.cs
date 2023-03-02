9f1f7842209915b1ca5afdd7f39dee5ce64d4eeb
Ivan Mijailovic <ivan.mijailovic994@gmail.com>
2023-03-02 09:56:51 +0100

    Clean up

--- Personal-finance-app/Controllers/HomeController.cs
+++ Personal-finance-app/Controllers/HomeController.cs
@@ -1,32 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Personal_finance_app.Models;
 using System.Diagnostics;
 
 namespace Personal_finance_app.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            
         }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Personal-finance-app/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Personal_finance_app.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
